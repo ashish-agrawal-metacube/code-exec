b5c20a9dafc4bdd3cbcc104c21cb805681d55053
ashish-agrawal-metacube <ashish.agrawal@metacube.com>
2018-09-06 21:49:52 +0530

    add language dropdown

--- app/assets/javascripts/angular-app/home/home-ctrl.js
+++ app/assets/javascripts/angular-app/home/home-ctrl.js
@@ -1,4 +1,24 @@
 angular.module('codeExecApp').controller('HomeCtrl', ['$scope', function ($scope ) {
 
+  $scope.langs = [{label: "C", mode: "c_cpp" },{label: "C++", mode: "c_cpp" },{label: "Java", mode: "java" }];
+
+  $scope.selectedLang = $scope.langs[1];
+
+  $scope.modeChanged = function(){
+    $scope.aceSession.setMode("ace/mode/"+$scope.selectedLang.mode);
+  };
+
+  $scope.aceOption = {
+          mode: "c_cpp",
+          theme:'chrome',
+          onLoad: function(_editor){
+            $scope.aceSession = _editor.getSession();
+
+            _editor.setOptions({ fontSize: "16px",displayIndentGuides: true});
+            _editor.$blockScrolling = Infinity;
+
+          }
+        };
+
 
 }]);
